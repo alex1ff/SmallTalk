570ff062bec6a28ed2b66d61e9bcbe16391e375a
Патрик Карденас <patrikkardenas@MacBook-Pro-Patrik.local>
2026-01-09 19:10:50 +0500

    Remove GlassNav and liquid_glass_renderer

--- lib/custom_code/widgets/index.dart
+++ lib/custom_code/widgets/index.dart
@@ -3,4 +3,3 @@ export 'proggres_bar.dart' show ProggresBar;
 export 'semi_circle_slider.dart' show SemiCircleSlider;
 export 'timepicker.dart' show Timepicker;
 export 'minimal_daily_widget.dart' show MinimalDailyWidget;
-export 'glass_nav.dart' show GlassNav;
