b806e41e7ea40bdacfa7d9545809a7d9cd5a06ff
Патрик Карденас <patrikkardenas@MacBook-Pro-Patrik.local>
2026-01-09 19:32:04 +0500

    Fix PushKit payload extra type in AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -71,7 +71,7 @@ import flutter_callkit_incoming
       handle: handle,
       type: isVideo ? 1 : 0
     )
-    data.extra = payloadDict
+    data.extra = NSDictionary(dictionary: payloadDict)
 
     SwiftFlutterCallkitIncomingPlugin.sharedInstance?.showCallkitIncoming(data, fromPushKit: true) {
       completion()
