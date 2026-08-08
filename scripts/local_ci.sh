77ca60ed477eecf36db970de04ad4aef372463b7
Патрик Карденас <patrikkardenas@MacBook-Pro-Patrik.local>
2026-08-08 19:11:04 +0500

    chore: scope dependency audit to runtime

--- scripts/local_ci.sh
+++ scripts/local_ci.sh
@@ -7,8 +7,8 @@ flutter pub get
 flutter analyze
 flutter test
 
-npm ci --prefix firebase/functions
-npm ci --prefix firebase/custom_cloud_functions
+npm ci --prefix firebase/functions --audit=false
+npm ci --prefix firebase/custom_cloud_functions --audit=false
 npm audit --prefix firebase/functions --omit=dev --audit-level=high
 npm audit --prefix firebase/custom_cloud_functions --omit=dev --audit-level=high
 npm run backend:ci
